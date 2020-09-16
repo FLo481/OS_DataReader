8f30277b6fcb5c22eaa0adf206e42699a41c854f
Flo <florian.deman@stud-mail.uni-wuerzburg.de>
2020-09-17 00:16:55 +0200

    - implemented optimized read in - fit function now fits all data sets -- fit needs to be optimized though - plot shows angular resolution over slit width

--- OS_DataReader.py
+++ OS_DataReader.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize
+import re
 
 def Lorentz_func (x, y0, a, Gamma, xc):
 
@@ -10,101 +11,127 @@ def Lorentz_func (x, y0, a, Gamma, xc):
 
 def reader (dirName):
 
-    data = []
-    intensity = []
-    wavelength = []
+    data = {}
+    slit_width = [] 
+    i = 0
+    j = 0
     
     for root, dirs , files in os.walk(dirName):
         for file in files:
             if file.endswith(".asc"):
+                slit_width.append(re.findall('\d+', str(file))[1])
                 print("Reading in file " + os.path.join(root, file))
                 with open(os.path.join(root, file)) as f:
                     reader = csv.reader(f, delimiter = '\t')
                     for row in reader:
-                        data.append(row)
+                        data[j, i] = row
+                        i += 1
+                i = 0
+                j += 1
 
     f.close()
 
-    #print(data[1023][0])
-    #print(data[1][0])
-    #print(data[1023][1024])
+    #explanation of indexing
 
-    #for i in range(0, 1024):
-    #    for j in range(1, 1025):
-    #        wavelength.append(data[i][0])
-    #        intensity.append(data[i][j])
+    #data[number of file, number of line][number of entry in line]
 
-    for i in range(0, 1024):
-        wavelength.append(data[i][0])
-        intensity.append(data[i][513])
+    return data, j, slit_width
 
+def fit_spectrum(dirName):
 
-
-
-    return intensity, wavelength
-
-def plot_spectrum():
-
-    Angular_res_folder = r"C:\Users\Flo\Desktop\F Praktikum\OS\Daten\AV"
-    intensity, wavelength = reader(Angular_res_folder)
+    length = 1024  
+    data, files, slit_width = reader(dirName)
+    x_plt = np.empty([files, length], dtype = float)
+    y_plt = np.empty([files, length], dtype = float)
+    y_err = np.empty([files, length], dtype = float)
+    params = [0]*files
+    params_cov = [0]*files
+    y_max = []
     x_min = 817
     x_max = 821
+
+    for j in range(0, files):
+        for i in range(0, length):
+            x_plt[j,i] = data[j,i][0]
+            y_plt[j,i] = data[j,i][512]
+        y_max.append(max(y_plt[j]))
     
-    intensity = list(map(float, intensity))
-    wavelength = list(map(float, wavelength))
+    for j in range(0, files):
+        for i in range(0, length):
+            y_err[j,i] = y_plt[j,i]/np.sqrt(y_plt[j,i])
+            y_plt[j,i] = y_plt[j,i]/y_max[j]
 
-    y_max = max(intensity)
+    for i in range(0, files):
+        params[i], params_cov[i] = scipy.optimize.curve_fit(Lorentz_func, x_plt[i], y_plt[i], p0 = [0.01,0.1,0.1,x_min], bounds = ([0.01,0,0.1,x_min],[0.05,1,1.5,x_max]), sigma = y_err[i], absolute_sigma = True)
+        print("x_c = ", params[i][3], " FWHM = ", 2*params[i][2], " Angular resolution = ", params[i][3]/(2*params[i][2]))
+        
+    #compute Chi squared if necessary
 
-    x_plt = np.empty(len(wavelength), dtype = float)
-    y_plt = np.empty(len(intensity), dtype = float)
-    x_plt1 = np.empty([], dtype = float)
-    y_plt1 = np.empty([], dtype = float)
-    y_err = np.empty(len(intensity), dtype = float)
-    y_err1 = np.empty([], dtype = float)
+    #chi_squared = [0]*files
+    #fit = np.empty([files, length], dtype = float)
 
-    x_plt[:] = wavelength
-    y_plt[:] = intensity
+    #for i in range(0, files):
+    #    fit[i] = Lorentz_func(x_plt[i], params[i][0], params[i][1], params[i][2], params[i][3])
+    
+    #for j in range(0, files):
+    #    for i in range(0, length):
+    #        chi_squared[j] += (y_plt[j,i] - fit[j,i])**2/(y_err[j,i])**2
 
-    for i in range(0,len(intensity)):
-        y_plt[i] = y_plt[i]/y_max
-        y_err[i] = y_plt[i]/(np.sqrt(y_plt[i]))
+    #for i in range(0, files):
+    #    print("x_c = ", params[i][3], " FWHM = ", 2*params[i][2], " Angular resolution = ", params[i][3]/(2*params[i][2]), "red. chi squared =", chi_squared[i]/(len(x_plt[i])-4))
 
-    for i in range(0,len(intensity)):
-        if x_max > x_plt[i] > x_min:
-            x_plt1 = np.append(x_plt, x_plt[i])
-            y_plt1 = np.append(y_plt, y_plt[i])
-            y_err1 = np.append(y_err, y_err[i])
+    
+    #plt.errorbar(x_plt[0], y_plt[0], yerr = None, fmt = 'o', markersize = 1)
+    #plt.plot(x_plt[0], Lorentz_func(x_plt[0], params[0][0], params[0][1], params[0][2], params[0][3]))
+    #plt.grid()
+    #plt.show()
+    #plt.clf()
 
-    fit = np.empty(len(x_plt1), dtype = float)
+    return params, params_cov, slit_width
 
-    plt.errorbar(x_plt, y_plt, yerr = None, fmt = 'o', markersize = 1)
-    params, params_cov = scipy.optimize.curve_fit(Lorentz_func, x_plt1, y_plt1, p0 = [0.04,0.1,0.1,x_min], bounds = ([0.04,0,0,x_min],[0.05,1,1,x_max]), sigma = None, absolute_sigma = True)
-    plt.plot(x_plt, Lorentz_func(x_plt, params[0], params[1], params[2], params[3]))
+def calc_ang_res():
 
-    print("x_c = ", params[3], " FWHM = ", 2*params[2], " Angular resolution = ", params[3]/(2*params[2]))
+    Grating_150_dir = r"C:\Users\Flo\Desktop\F Praktikum\OS\Daten\150"
+    Grating_300_dir = r"C:\Users\Flo\Desktop\F Praktikum\OS\Daten\300"
+    Grating_1200_dir = r"C:\Users\Flo\Desktop\F Praktikum\OS\Daten\1200"
 
-    #calculation Chi squared
+    params, params_cov, slit_width = fit_spectrum(Grating_150_dir)
 
-    chi_squared = 0
-    fit[:] = Lorentz_func(x_plt1, params[0], params[1], params[2], params[3])
+    params1, params_cov1, slit_width1 = fit_spectrum(Grating_300_dir)
 
-    for i in range(0, len(x_plt1)):
-        chi_squared += (y_plt1[i] - fit[i])**2/(y_err1[i])**2
+    params2, params_cov2, slit_width2 = fit_spectrum(Grating_1200_dir)
 
-    print("red. Chi squared : ", chi_squared/(len(x_plt1)-4))
+    x_plt = np.empty(len(slit_width), dtype = float)
+    y_plt = np.empty(len(slit_width), dtype = float)
+    x_plt1 = np.empty(len(slit_width1), dtype = float)
+    y_plt1 = np.empty(len(slit_width1), dtype = float)
+    x_plt2 = np.empty(len(slit_width2), dtype = float)
+    y_plt2 = np.empty(len(slit_width2), dtype = float)
 
+    x_plt[:] = slit_width
+    x_plt1[:] = slit_width1
+    x_plt2[:] = slit_width2
+
+    for i in range(0, len(slit_width)):
+        y_plt[i] = params[i][3]/(2*params[i][2])
+        y_plt1[i] = params1[i][3]/(2*params1[i][2])
+        y_plt2[i] = params2[i][3]/(2*params2[i][2])
+
+    plt.errorbar(x_plt, y_plt, yerr = None, fmt = 'x', markersize = 5)
+    plt.errorbar(x_plt1, y_plt1, yerr = None, fmt = 'x', markersize = 5)
+    plt.errorbar(x_plt2, y_plt2, yerr = None, fmt = 'x', markersize = 5)
 
     plt.grid()
-    #plt.xlim(x_min, x_max)
-    #plt.ylim(0, 0.4)
-
     plt.show()
     plt.clf()
 
     return 0
-
+   
 def main():
-    plot_spectrum()
+    #test = r"C:\Users\Flo\Desktop\F Praktikum\OS\Daten\AV"
+    #reader(test)
+    #fit_spectrum(test)
+    calc_ang_res()
 
 if __name__ == "__main__" :
     main()
