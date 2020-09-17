924a8971c843523ea74799abe6e6a9b8565bbe5b
Flo <florian.deman@stud-mail.uni-wuerzburg.de>
2020-09-17 13:04:25 +0200

    - added functionality to plot every fit for each grating plus respective slit width -- i.e. added plot_spectrum() function - initial values and bounds can now be adjusted individually for each grating

--- OS_DataReader.py
+++ OS_DataReader.py
@@ -37,6 +37,27 @@ def reader (dirName):
 
     return data, j, slit_width
 
+def plot_spectrum(x_plt, y_plt, y0, a, Gamma, xc, grating, slit_width):
+
+    if slit_width == 0:
+        n = 100
+    elif slit_width == 1:
+        n = 10
+    elif slit_width == 2:
+        n = 25
+    elif slit_width == 3:
+        n = 50
+
+    plt.errorbar(x_plt, y_plt, yerr = None, fmt = 'o', markersize = 1)
+    plt.title('Grating : {}, Slit width : {}' .format(grating, n))
+    plt.plot(x_plt, Lorentz_func(x_plt, y0, a, Gamma, xc), '-b')
+    
+    plt.grid()
+    plt.show()
+    plt.clf()
+
+    return 0
+
 def fit_spectrum(dirName):
 
     length = 1024  
@@ -61,8 +82,20 @@ def fit_spectrum(dirName):
             y_err[j,i] = y_plt[j,i]/np.sqrt(y_plt[j,i])
             y_plt[j,i] = y_plt[j,i]/y_max[j]
 
+    #use different initial values and bounds for different gratings
+
+    if int(re.findall('\d+', str(dirName))[0]) == 150: #good
+        intial_values = [0.01,0.1,0.1,x_min]
+        bounds = ([0.01,0,0.01,x_min],[0.1,1,2,x_max])
+    elif int(re.findall('\d+', str(dirName))[0]) == 300: #good
+        intial_values = [0.01,0.1,0.1,x_min]
+        bounds = ([0.01,0,0.01,x_min],[0.1,1,2,x_max])
+    elif int(re.findall('\d+', str(dirName))[0]) == 1200: #good
+        intial_values = [0.01,0.1,0.01,x_min]
+        bounds = ([0.01,0,0.01,x_min],[0.1,1,1,x_max])
+
     for i in range(0, files):
-        params[i], params_cov[i] = scipy.optimize.curve_fit(Lorentz_func, x_plt[i], y_plt[i], p0 = [0.01,0.1,0.1,x_min], bounds = ([0.01,0,0.1,x_min],[0.05,1,1.5,x_max]), sigma = y_err[i], absolute_sigma = True)
+        params[i], params_cov[i] = scipy.optimize.curve_fit(Lorentz_func, x_plt[i], y_plt[i], p0 = intial_values, bounds = bounds, sigma = y_err[i], absolute_sigma = True)
         print("x_c = ", params[i][3], " FWHM = ", 2*params[i][2], " Angular resolution = ", params[i][3]/(2*params[i][2]))
         
     #compute Chi squared if necessary
@@ -80,14 +113,7 @@ def fit_spectrum(dirName):
     #for i in range(0, files):
     #    print("x_c = ", params[i][3], " FWHM = ", 2*params[i][2], " Angular resolution = ", params[i][3]/(2*params[i][2]), "red. chi squared =", chi_squared[i]/(len(x_plt[i])-4))
 
-    
-    #plt.errorbar(x_plt[0], y_plt[0], yerr = None, fmt = 'o', markersize = 1)
-    #plt.plot(x_plt[0], Lorentz_func(x_plt[0], params[0][0], params[0][1], params[0][2], params[0][3]))
-    #plt.grid()
-    #plt.show()
-    #plt.clf()
-
-    return params, params_cov, slit_width
+    return params, params_cov, slit_width, x_plt, y_plt
 
 def calc_ang_res():
 
@@ -95,11 +121,22 @@ def calc_ang_res():
     Grating_300_dir = r"C:\Users\Flo\Desktop\F Praktikum\OS\Daten\300"
     Grating_1200_dir = r"C:\Users\Flo\Desktop\F Praktikum\OS\Daten\1200"
 
-    params, params_cov, slit_width = fit_spectrum(Grating_150_dir)
+    params, params_cov, slit_width, x, y = fit_spectrum(Grating_150_dir)
 
-    params1, params_cov1, slit_width1 = fit_spectrum(Grating_300_dir)
+    #n = 1
+    #plot_spectrum(x[n], y[n], params[n][0], params[n][1], params[n][2], params[n][3], 150, n)
 
-    params2, params_cov2, slit_width2 = fit_spectrum(Grating_1200_dir)
+    params1, params_cov1, slit_width1, x1, y1 = fit_spectrum(Grating_300_dir)
+
+    #n1 = 1
+    #plot_spectrum(x1[n1], y1[n1], params1[n1][0], params1[n1][1], params1[n1][2], params1[n1][3], 300, n1)
+
+    params2, params_cov2, slit_width2, x2, y2 = fit_spectrum(Grating_1200_dir)
+
+    #n2 = 2
+    #plot_spectrum(x2[n2], y2[n2], params2[n2][0], params2[n2][1], params2[n2][2], params2[n2][3], 1200, n2)
+
+
 
     x_plt = np.empty(len(slit_width), dtype = float)
     y_plt = np.empty(len(slit_width), dtype = float)
@@ -117,10 +154,11 @@ def calc_ang_res():
         y_plt1[i] = params1[i][3]/(2*params1[i][2])
         y_plt2[i] = params2[i][3]/(2*params2[i][2])
 
-    plt.errorbar(x_plt, y_plt, yerr = None, fmt = 'x', markersize = 5)
-    plt.errorbar(x_plt1, y_plt1, yerr = None, fmt = 'x', markersize = 5)
-    plt.errorbar(x_plt2, y_plt2, yerr = None, fmt = 'x', markersize = 5)
+    #plt.errorbar(x_plt, y_plt, yerr = None, fmt = 'x', markersize = 5, label = 'Grating 150')
+    #plt.errorbar(x_plt1, y_plt1, yerr = None, fmt = 'x', markersize = 5, label = 'Grating 300')
+    #plt.errorbar(x_plt2, y_plt2, yerr = None, fmt = 'x', markersize = 5, label = 'Grating 1200')
 
+    plt.legend()
     plt.grid()
     plt.show()
     plt.clf()
